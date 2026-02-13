88e3e97a786afb574a1a38cf818250caea4e8445
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2026-02-13 11:37:11 +0100

    downgrade to jest 29, same as kibana

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   // Only treat *.test.ts files as test suites (excludes fixtures, helpers, etc. in __tests__/)
