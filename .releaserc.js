de0b75e688a20350d70be3ff8f41981dfa41ce77
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2026-02-16 11:12:50 +0100

    configure semantic release

---
+++ .releaserc.js
@@ -0,0 +1,48 @@
+const isDryRun = process.argv.includes('--dry-run');
+
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'feature', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ...(isDryRun
+      ? []
+      : [
+          [
+            '@semantic-release/github',
+            {
+              successComment: false,
+              failComment: false,
+            },
+          ],
+          '@semantic-release/npm',
+        ]),
+  ],
+};
