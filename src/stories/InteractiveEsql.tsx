bc191f657992171b31364ed7275845412fe56364
vadimkibana <vadimkibana@gmail.com>
2026-03-13 12:01:49 +0100

    final adjustments

--- src/stories/InteractiveEsql.tsx
+++ src/stories/InteractiveEsql.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { WithSlider } from './WithSlicer';
+import { WithSlider } from './WithSlider';
 import { FormatEsql, FormatEsqlProps } from './FormatEsql';
 
 export interface InteractiveEsqlProps extends FormatEsqlProps {
