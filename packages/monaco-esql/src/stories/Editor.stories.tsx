1122df2e55a370666663d49827204d8c1ffc4118
vadimkibana <vadimkibana@gmail.com>
2026-07-09 14:45:32 +0200

    test: update Storybook story

--- packages/monaco-esql/src/stories/Editor.stories.tsx
+++ packages/monaco-esql/src/stories/Editor.stories.tsx
@@ -7,43 +7,39 @@
  * not use this file except in compliance with the License.
  */
 
-import * as React from "react";
-import type { Meta, StoryObj } from "@storybook/react-vite";
-import Editor from "@monaco-editor/react";
-import example from "../../example.esql?raw";
-import { language } from "../monarch-shared";
-import { promLanguageDefinition } from "monaco-promql";
+/// <reference types="vite/client" />
+
+import * as React from 'react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import Editor from '@monaco-editor/react';
+import example from '../../example.esql?raw';
+import { language } from '../monarch-shared';
+import { promLanguageDefinition } from 'monaco-promql';
 
 const meta: Meta = {
-	title: "Editor",
+  title: 'MonacoEsql/Editor',
 };
 
 export default meta;
 const promqlLanguageId = promLanguageDefinition.id;
 export const Primary: StoryObj<typeof meta> = {
-	render: () => (
-		<div>
-			<Editor
-				height="90vh"
-				defaultLanguage="esql"
-				language="esql"
-				defaultValue={example}
-				onMount={(_, monaco) => {
-					monaco.languages.register({ id: "esql" });
-					monaco.languages.register(promLanguageDefinition);
-					monaco.languages.setMonarchTokensProvider("esql", language);
-					promLanguageDefinition.loader().then((mod) => {
-						monaco.languages.setMonarchTokensProvider(
-							promqlLanguageId,
-							mod.language,
-						);
-						monaco.languages.setLanguageConfiguration(
-							promqlLanguageId,
-							mod.languageConfiguration,
-						);
-					});
-				}}
-			/>
-		</div>
-	),
+  render: () => (
+    <div>
+      <Editor
+        height="90vh"
+        defaultLanguage="esql"
+        language="esql"
+        defaultValue={example}
+        onMount={(_, monaco) => {
+          monaco.languages.register({ id: 'esql' });
+          monaco.languages.register(promLanguageDefinition);
+          monaco.languages.setMonarchTokensProvider('esql', language);
+          promLanguageDefinition.loader().then((mod) => {
+            monaco.languages.setMonarchTokensProvider(promqlLanguageId, mod.language);
+            monaco.languages.setLanguageConfiguration(promqlLanguageId, mod.languageConfiguration);
+          });
+        }}
+      />
+    </div>
+  ),
 };
