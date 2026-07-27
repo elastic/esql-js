6dbbb30afe680e46b1f2a6eeb7eb1170cd6f1063
vadimkibana <vadimkibana@gmail.com>
2026-07-27 18:22:45 +0200

    refactor: import from package

--- packages/esql/src/ast/walker/helpers.ts
+++ packages/esql/src/ast/walker/helpers.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { PromQLAstNode } from '../../embedded_languages/promql/types';
+import type { PromQLAstNode } from '@elastic/esql-types';
 import type {
   ESQLAstCommand,
   ESQLAstQueryExpression,
